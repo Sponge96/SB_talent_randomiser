e907acdba1c7c4e743a5b81bfc5022eacf96f9bc
Sponge96 <jackp.bcot@gmail.com>
2021-06-21 10:21:28 +0100

    Update SB_talent_randomiser.py
    - Updated talents dictionary
    - Updated MAX_SPEND to 7 
    - Updated rolls to match new 'cost'

--- SB_talent_randomiser.py
+++ SB_talent_randomiser.py
@@ -2,44 +2,49 @@ import random
 
 while True:
     talents = {
-        'Focused Mana':
+        'Intelligent':
             {'tree': "mind", 'cost': 1},
+        'Curious':
+            {'tree': "mind", 'cost': 2},
         'Spellslinger':
-            {'tree': "mind", 'cost': 1},
-        'Harmony':
-            {'tree': "mind", 'cost': 3},
+            {'tree': "mind", 'cost': 2},
         'Tracking':
-            {'tree': "mind", 'cost': 2},
-        'Runic Fluency':
-            {'tree': "mind", 'cost': 2},
-        'Vigor':
             {'tree': "mind", 'cost': 3},
+        'Hardened':
+            {'tree': "mind", 'cost': 3},
+        'Runic Fluency':
+            {'tree': "mind", 'cost': 4},
+        'Rebirth':
+            {'tree': "mind", 'cost': 4},
+        'Tough':
+            {'tree': "body", 'cost': 1},
         'Dexterity':
-            {'tree': "body", 'cost': 1},
-        'Scavenging':
-            {'tree': "body", 'cost': 1},
-        'Fervor':
-            {'tree': "body", 'cost': 3},
-        'Finders Keepers':
-            {'tree': "body", 'cost': 1},
-        'Fortitude':
             {'tree': "body", 'cost': 2},
-        'Foresight':
+        'Wellspring':
+            {'tree': "body", 'cost': 2},
+        'Fellowship':
             {'tree': "body", 'cost': 3},
-        'Escapist':
+        'Vigor':
+            {'tree': "body", 'cost': 3},
+        'Ambidextrous':
+            {'tree': "body", 'cost': 4},
+        'Athletic':
+            {'tree': "body", 'cost': 4},
+        'Mystical':
             {'tree': "spirit", 'cost': 1},
-        'Recklessness':
-            {'tree': "spirit", 'cost': 1},
-        'Recovery':
+        'Vampiric':
             {'tree': "spirit", 'cost': 2},
         'Thirsty':
             {'tree': "spirit", 'cost': 2},
-        'Vital Stone':
+        'Fortitude':
             {'tree': "spirit", 'cost': 3},
-        'Ambidextrous':
-            {'tree': "spirit", 'cost': 3}
+        'Inspired':
+            {'tree': "spirit", 'cost': 3},
+        'Foresight':
+            {'tree': "spirit", 'cost': 4},
+        'Gilded':
+            {'tree': "spirit", 'cost': 4}
     }
-
 
     def unpack_tree(to_unpack, tree):
         unpacked_tree = {key: value for (key, value) in to_unpack.items() if value['tree'] == tree}
@@ -55,14 +60,14 @@ while True:
     body_talent = unpack_tree(talents, 'body')
     spirit_talent = unpack_tree(talents, 'spirit')
 
-    first_roll = random.randint(0, 3)
-    second_roll = random.randint(0, 3)
+    first_roll = random.randint(0, 4)
+    second_roll = random.randint(0, 4)
 
-    MAX_SPEND = 6  # The cost of talents cannot exceed the MAX_SPEND otherwise the choices become incompatible
+    MAX_SPEND = 7  # The cost of talents cannot exceed the MAX_SPEND otherwise the choices become incompatible
     remaining_spend = MAX_SPEND - (first_roll + second_roll)
 
     if remaining_spend >= 4:
-        third_roll = random.randint(0, 3)
+        third_roll = random.randint(0, 4)
 
     else:
         third_roll = random.randint(0, remaining_spend)
